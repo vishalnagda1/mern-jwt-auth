bc9c29ee46b85731cee427755bfa42e690ceedaa
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-01 15:20:20 +0530

    pull required dependencies and load input validation and user model

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Load User model
+const User = require("../../models/User");
