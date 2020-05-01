29684f8246e1ca9f2a237f800af706dff5b22d79
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-01 16:59:35 +0530

    update server js - import passpost and user routes, add passport as middleware, configure passport and add user routes to app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -8,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Congig
+// DB Config
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
@@ -16,6 +18,15 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
