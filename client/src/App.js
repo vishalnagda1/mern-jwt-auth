9c0b29009d38762632c5d29ac88f9bbf4bacae29
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-16 20:55:39 +0530

    fix the router lib issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import { Router } from "react-router-dom";
 
 function App() {
   return (
