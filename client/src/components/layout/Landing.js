fa2087fb5ff517ae7406af49cab68256e290cfd3
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-16 18:58:29 +0530

    fix the router issue

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 class Landing extends Component {
   render() {
     return (
@@ -16,32 +16,34 @@ class Landing extends Component {
               passport and JWTs
             </p>
             <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
-              </Link>
-            </div>
+            <Router>
+              <div className="col s6">
+                <Link
+                  to="/register"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Register
+                </Link>
+              </div>
+              <div className="col s6">
+                <Link
+                  to="/login"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large btn-flat waves-effect white black-text"
+                >
+                  Log In
+                </Link>
+              </div>
+            </Router>
           </div>
         </div>
       </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{ fontFamily: "monospace" }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              MERN
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <Router>
+        <div className="navbar-fixed">
+          <nav className="z-depth-0">
+            <div className="nav-wrapper white">
+              <Link
+                to="/"
+                style={{ fontFamily: "monospace" }}
+                className="col s5 brand-logo center black-text"
+              >
+                <i className="material-icons">code</i>
+                MERN
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </Router>
     );
   }
 }
