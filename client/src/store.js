a0264b5d8c3aa500254225071f494657fabf2fca
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-16 22:27:53 +0530

    create store with empty rootReducer

---
+++ client/src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  () => [],
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+export default store;
