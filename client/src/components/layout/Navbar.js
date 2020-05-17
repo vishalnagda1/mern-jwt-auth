940f196bbf2a1d061b8fe3ca64e904e090f71b1d
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-17 16:54:59 +0530

    add material icons and update the project name in Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends Component {
               className="col s5 brand-logo center black-text"
             >
               <i className="material-icons">code</i>
-              MERN
+              MERN JWT Auth
             </Link>
           </div>
         </nav>
