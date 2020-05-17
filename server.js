44f606c25f8761ac37dfaa83c3b9c278edc0a809
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-18 00:18:24 +0530

    update server.js to server static files in production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 const users = require("./routes/api/users");
 
 const app = express();
@@ -27,6 +28,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
