414e79e4dc60a00ed8574f1b0154840a1c3d37aa
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-17 19:17:46 +0530

    code cleanup - remove unused router code from navbar and landing component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
