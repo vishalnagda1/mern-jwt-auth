39e363d22f762caf0b6ec290653c0cbe9ce403fe
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-17 19:12:02 +0530

    update login component - if logged in user tries to visit the login page then it should redirect to dashboard by default

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -15,6 +15,13 @@ class Login extends Component {
     };
   }
 
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push("/dashboard"); // push user to dashboard when they login
