1f2efe1b59ab6bcc7c2a82e68fc7b4c03e9b4609
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-17 16:05:48 +0530

    fix index reducer lib import type

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import errorReducer from "./errorReducer";
+import authReducer from "./authReducers";
+import errorReducer from "./errorReducers";
 
 export default combineReducers({
   auth: authReducer,
